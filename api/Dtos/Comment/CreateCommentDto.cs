073224e031dc43716ea3e0f45a72649789a7e1ae
hazyy <kamil.khaziev.03@gmail.com>
2025-08-14 16:16:54 +0300

    Первый коммит: базовая структура проекта

---
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.Comment;
+
+public class CreateCommentDto
+{
+    [Required]
+    [MinLength(5, ErrorMessage = "Tittle must be at least 5 characters")]
+    [MaxLength(280, ErrorMessage = "Tittle can not be over 280 characters")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(5, ErrorMessage = "Content must be at least 5 characters")]
+    [MaxLength(280, ErrorMessage = "Content can not be over 280 characters")]
+    public string Content { get; set; } = string.Empty;
+}
---
+++ api/Migrations/20250701153731_CommentOneToOne.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "408cc67d-c647-4311-9224-8ad29a86a6e7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f4766fdf-3fca-4e77-92c6-0988065b6cd2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "30441778-47d2-47d1-b07d-4ff399458dd7", null, "User", "USER" },
+                    { "99169941-73b6-49f6-91b8-e02faa878d9a", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "30441778-47d2-47d1-b07d-4ff399458dd7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "99169941-73b6-49f6-91b8-e02faa878d9a");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Comments");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "408cc67d-c647-4311-9224-8ad29a86a6e7", null, "User", "USER" },
+                    { "f4766fdf-3fca-4e77-92c6-0988065b6cd2", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
